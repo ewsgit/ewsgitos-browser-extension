591dac4ea65c6c26c8b3319cbfbd702d3cade774
ewsgit <ewsgit@duck.com>
2023-02-06 21:28:14 +0000

    add more styling to microsoft login and sparx maths

---
+++ background.js
@@ -0,0 +1,42 @@
+// "content_scripts": [
+//     {
+//         "matches": ["https://*.youtube.com/*"],
+//         "css": ["./youtube.com/all.css"]
+//     },
+//     {
+//         "matches": ["https://searx.be/"],
+//         "css": ["./searx.be/index.css"]
+//     },
+//     {
+//         "matches": ["https://searx.be/search"],
+//         "css": ["./searx.be/search.css"]
+//     },
+//     {
+//         "matches": ["https://www.google.com/*", "https://www.google.co.uk/*"],
+//         "css": ["./google.com/all.css"]
+//     },
+//     {
+//         "matches": ["https://www.sparxmaths.uk/student"],
+//         "js": ["./sparxmaths.uk/bookwork.js"]
+//     },
+//     {
+//         "matches": ["https://twitter.com/*"],
+//         "css": ["./twitter.com/all.css"]
+//     },
+//     {
+//         "matches": ["https://manchesterworld.uk/*"],
+//         "css": ["./manchesterworld.uk/all.css"]
+//     },
+//     {
+//         "matches": ["https://drafts.csswg.org/*"],
+//         "css": ["./csswg.org/drafts.css"]
+//     },
+//     {
+//         "matches": ["https://*.sharepoint.com/*"],
+//         "css": ["./sharepoint.com/all.css"]
+//     },
+//     {
+//         "matches": ["https://*.twitch.tv/*"],
+//         "css": ["./twitch.tv/all.css"]
+//     }
+// ]
