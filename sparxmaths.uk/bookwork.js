2deed0e8079d238fc09f3ccdba5d2f5cd7ee6229
ewsgit <ewsgit@duck.com>
2023-03-19 12:01:16 +0000

    general: general changes sparx

--- sparxmaths.uk/bookwork.js
+++ sparxmaths.uk/bookwork.js
@@ -4,7 +4,71 @@
 
 function main() {
     console.log(`EwsgitOS: sparxmaths hooked`)
-    chrome.storage.local.set({ sparxPreviousAnswers: { test: 1 } }, null)
+
+//     sample answers
+//     console.log(parseAnswer(`<div class="keyboard-focus-start-point" tabindex="-1"></div>
+// <div class="answer-part answer-part-outer" style="left: 590px;">
+//     <div class="answer-part">
+//         <div class="gap-container selected">
+//             <div class="gap-container-bridge" style="z-index: 3; right: 9px;"></div>
+//             <div class="keypad-number-input" style="width: 154px; height: 114px;" tabindex="0"><input
+//                     autocapitalize="off" autocomplete="off" autocorrect="off" class="number-input" maxlength="50" readonly=""
+//                     size="15" spellcheck="false" tabindex="-1" type="text" value="90">
+//                 <span class="keypad-animation-container" style="left: -108px;">
+// <div class="keypad-container"
+//      style="display: block; opacity: 1; transform-origin: 50% 50% 0px; transform: scaleX(1) scaleY(1);">
+// <div class="keypad keypad-grid">
+// <div class="keypad-row">
+// <div class="button-container">
+// <div class="keypad-button" data-test-target="7">
+// <span>7</span>
+// </div>
+// </div><div class="button-container"><div class="keypad-button" data-test-target="8"><span>8</span></div></div><div
+//         class="button-container"><div class="keypad-button" data-test-target="9"><span>9</span></div></div><div
+//         class="button-container"><div class="keypad-button keypad-button-backspace" data-test-target="Backspace"><img
+//         alt="back button"
+//         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAeCAYAAABXNvynAAAACXBIWXMAAAsSAAALEgHS3X78AAABrUlEQVRYhe2XwW2DMBiFv1TVf21HaDdINggbZIPCAEjNBO0GqeQBYBM6AiNkhPTqS3uwiYACMbYrVIl3s2SePszzM4ZVqzrahDysRA7ANhLLmC5AmWt9Abj3cVAij8AJSONxTeoF2IEHsBLZAgVmZUvg2Lz9X0iJnIDXZjwLWImkmJUFyHKty2hk4/pqD5yAexGoMbB1dDQH3d2aYCNQYWBLIFkKFm6s8EIRmNTgCiuRRyVSYDbXGbOqpaupEvm2z0/NSe28vTvuAHCkCNRAOgZtv1xh583y7gBbowp4wkQg86yshBHoHmwy1/8KbPvOKwJ9WYhf0KGwHWBMOX8SqQUGoKtQWHCotRC1oM/AHvNf4A0LXeAPa1rZjRdLB8yeAGgOIG9dgXOtj0BmzSubtyD1MvvMjfZwUScSdqM1n7BQIoU9lkNhk1zrMxPt4QVsoWtrXGK6eHZExtpgrD2CgBvjXOsM/4iMtsEA9D4YuGVe4heRZAi25dtAN+DOcroiLfl7qUTegbdc6w049nCEiETTrIMjZovM0EN74HVrXuASWuda7+AfXvNXrerpBwSP8Fw3B2WtAAAAAElFTkSuQmCC"></div></div></div><div
+//         class="keypad-row"><div class="button-container"><div class="keypad-button" data-test-target="4"><span>4</span></div></div><div
+//         class="button-container"><div class="keypad-button" data-test-target="5"><span>5</span></div></div><div
+//         class="button-container"><div class="keypad-button" data-test-target="6"><span>6</span></div></div><div
+//         class="button-container"><div class="keypad-button" data-test-target="-"><span>−</span></div></div></div><div
+//         class="keypad-row"><div class="button-container"><div class="keypad-button" data-test-target="1"><span>1</span></div></div><div
+//         class="button-container"><div class="keypad-button" data-test-target="2"><span>2</span></div></div><div
+//         class="button-container"><div class="keypad-button" data-test-target="3"><span>3</span></div></div><div
+//         class="button-container"></div></div><div class="keypad-row"><div class="button-container wide-button"><div
+//         class="keypad-button" data-test-target="0"><span>0</span></div></div><div class="button-container"><div
+//         class="keypad-button" data-test-target="."><span>.</span></div></div><div class="button-container"></div></div></div></div></span>
+//             </div>
+//         </div>
+//         <div><span class="text"><span class="katex"><span class="katex-mathml"><math><semantics><mrow><msup><mrow></mrow><mo>∘</mo></msup></mrow><annotation
+//                 encoding="application/x-tex">{^\\circ}</annotation></semantics></math></span><span aria-hidden="true"
+//                                                                                                   class="katex-html"><span
+//                 class="base"><span class="strut" style="height:0.674115em;vertical-align:0em;"></span><span
+//                 class="mord"><span class="mord"><span></span><span class="msupsub"><span class="vlist-t"><span
+//                 class="vlist-r"><span class="vlist" style="height:0.674115em;"><span
+//                 style="top:-3.063em;margin-right:0.05em;"><span class="pstrut" style="height:2.7em;"></span><span
+//                 class="sizing reset-size6 size3 mtight"><span
+//                 class="mbin mtight">∘</span></span></span></span></span></span></span></span></span></span></span></span></span>
+//         </div>
+//     </div>
+// </div>
+// <div class="entry-area" style="width: 1389px; height: 383px; top: -1px;">
+//     <div>
+//         <div class="entry-area-bubble"
+//              style="border-top-right-radius: 5px; border-top-left-radius: 5px; left: 471px; width: 428px; height: 363px; padding: 0px; top: 0px; display: flex; position: absolute; flex-flow: column nowrap;"></div>
+//     </div>
+// </div>
+// `))
+//
+//     console.log(parseAnswer(`<div class="choices">
+//     <div class="choice choice-text" tabindex="0">
+//         <div><span class="text">clockwise</span></div>
+//     </div>
+//     <div class="choice choice-text selected" tabindex="0">
+//         <div><span class="text">anticlockwise</span></div>
+//     </div>
+// </div>
+// `))
 
     if (!document.getElementById("app-container")) setTimeout(() => main(), 1000)
 
@@ -20,12 +84,44 @@ function main() {
         if (!data) {
             previousAnswers = {}
         } else {
-            previousAnswers = data
+            previousAnswers = data.sparxPreviousAnswers || {}
             console.log("synced: ", previousAnswers)
         }
     })
 
     new MutationObserver((mutations, observer) => {
+
+        if (document.getElementById("answer-wac-box")) {
+            let wacCode = ""
+            wacCode = document.querySelector("#answer-wac-box > div > div > div.wac-text-container > div.bookwork-code").innerHTML.replaceAll("Bookwork code: ", "").replaceAll("</span>", "")
+
+            let popup;
+
+            if (!document.getElementById("wac-popup-123")) {
+                popup = document.createElement("div")
+                document.body.appendChild(popup)
+                popup.id = "wac-popup-123"
+                popup.style.right = "0.5rem"
+                popup.style.top = "calc(55px + 0.5rem)"
+                popup.style.backgroundColor = "#eeeeee"
+                popup.style.borderRadius = "1rem"
+                popup.style.position = "fixed"
+                popup.style.zIndex = "100000"
+                popup.style.padding = "1rem 2rem"
+            } else {
+                popup = document.getElementById("wac-popup-123")
+            }
+
+            popup.innerHTML = (previousAnswers[wacCode]?.parsed?.toString().replaceAll(",", "<br>")) || "The requested code doesn't exist ;("
+
+            document.getElementById("bottom-bar").onclick = () => {
+                popup.remove()
+            }
+
+            return
+        } else {
+            if (document.getElementById("wac-popup-123")) document.getElementById("wac-popup-123").remove()
+        }
 
         let middle = document.querySelectorAll("#top-bar > div")[0]
 
@@ -38,9 +134,11 @@ function main() {
                     document.getElementById(`skill-delivery-submit-button`).onclick = () => {
                         questionData.answer = document.querySelectorAll(`.answer`)[0].innerHTML
                         if (document.querySelector(`.answer`)) {
-                            previousAnswers[middle.children[0].innerHTML.replaceAll("<span>Bookwork code: ", "").replaceAll("</span>", "")] = document.querySelectorAll(`.answer`)[0].innerHTML
-                            chrome.storage.local.set({ sparxPreviousAnswers: previousAnswers }, null)
-                            console.log("found purely the answer")
+                            previousAnswers[middle.children[0].innerHTML.replaceAll("<span>Bookwork code: ", "").replaceAll("</span>", "")] = {
+                                // raw: document.querySelectorAll(`.answer`)[0].innerHTML,
+                                parsed: parseAnswer(document.querySelectorAll(`.answer`)[0].innerHTML)
+                            }
+                            chrome.storage.local.set({sparxPreviousAnswers: previousAnswers}, null)
                         }
                         console.table(previousAnswers)
                     }
@@ -54,9 +152,7 @@ function main() {
             }
         }
 
-        if(document.getElementById("#answer-wac-box")) return console.log("wac")
-
-        if (middle.children[0].innerHTML.replaceAll("<span>Bookwork code: ", "").replaceAll("</span>", "") === questionData.bookworkCode) return
+        if (middle?.children?.[0]?.innerHTML?.replaceAll("<span>Bookwork code: ", "")?.replaceAll("</span>", "") === questionData.bookworkCode) return
 
         questionData.bookworkCode = middle.children[0].innerHTML.replaceAll("<span>Bookwork code: ", "").replaceAll("</span>", "")
 
@@ -72,8 +168,44 @@ function main() {
         questionData.question = document.querySelectorAll(`#app-container > div.screen > div.main-view > div > div > div > div.skill-delivery-view > div.view-body > div > div > div > div > div`)[0]
 
         console.log(questionData)
-      
-    }).observe(document.getElementById("app-container"), { subtree: true, childList: true })
+
+    }).observe(document.getElementById("app-container"), {subtree: true, childList: true})
 }
 
 main()
+
+function parseAnswer(answer) {
+    // use a random string to obstruct it's meaning
+    let node = document.createElement("div")
+    node.id = "askur83jjhef8HU8h3jfelkw_Button_Component_678"
+    document.body.appendChild(node)
+    const answerNode = document.getElementById("askur83jjhef8HU8h3jfelkw_Button_Component_678")
+    answerNode.innerHTML = answer
+    answerNode.style.position = "absolute"
+    answerNode.style.top = "50%"
+    answerNode.style.left = "50%"
+    answerNode.style.translate = "-50% -50%"
+    answerNode.style.pointerEvents = "none"
+    answerNode.style.display = "none"
+
+    const DETECTED_ANSWERS = []
+
+    answerNode.querySelectorAll(".keypad-number-input").forEach(keypad => {
+        let keypadInput = keypad.querySelector("input.number-input")
+
+        DETECTED_ANSWERS.push(keypadInput.value.toString())
+    })
+
+    answerNode.querySelectorAll(".choices").forEach(choice => {
+        let selectedChoise = choice.querySelector(".selected").querySelector("div").querySelector("span")
+        DETECTED_ANSWERS.push(selectedChoise.innerText)
+    })
+
+    answerNode.querySelectorAll(".gap-slot").forEach(slot => {
+        let selectedChoise = slot.querySelector(".selected").querySelector("div").querySelector("div").querySelector("span")
+        DETECTED_ANSWERS.push(selectedChoise.innerText)
+    })
+
+    answerNode.remove()
+    return DETECTED_ANSWERS
+}
